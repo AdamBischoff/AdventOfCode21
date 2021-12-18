99bb611e5dd214e2376607e032d1dd93270f9b16
Adam Bischoff <s174295@student.dtu.dk>
2021-12-18 20:34:17 +0100

    Day 1

---
+++ 1/Main.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode21._1
+{
+    class Main
+    {
+        public static void Program1()
+        {
+            int[] file = getFile();
+            int lastDepth = file[0];
+            int timesIncreased = 0;
+
+            foreach (var line in file)
+            {
+                int depth = line;
+
+                if (depth > lastDepth)
+                    timesIncreased++;
+
+                lastDepth = depth;
+            }
+            Console.WriteLine("Result = " + timesIncreased);
+        }
+
+        public static void Program2()
+        {
+            int[] file = getFile();
+            int timesIncreased = 0;
+
+            int lastDepth = file[0] + file[1] + file[2];
+
+            for (int i = 3; i < file.Length; i++)
+            {
+                int newDepth = lastDepth - file[i - 3] + file[i];
+
+                if (newDepth > lastDepth)
+                    timesIncreased++;
+
+                lastDepth = newDepth;
+            }
+
+            Console.WriteLine("Result = " + timesIncreased);
+        }
+
+        public static int[] getFile()
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\adam\git\AdventOfCode21\1\Data.txt");
+            List<int> res = new List<int>();
+            foreach (string line in lines)
+            {
+                res.Add(int.Parse(line));
+            }
+
+            return res.ToArray();
+        }
+    }
+}
---
+++ AdventOfCode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AdventOfCode21
+{
+    class AdventOfCode
+    {
+        static void Main(string[] args)
+        {
+            AdventOfCode21._1.Main.Program1();
+            AdventOfCode21._1.Main.Program2();
+        }
+    }
+}
